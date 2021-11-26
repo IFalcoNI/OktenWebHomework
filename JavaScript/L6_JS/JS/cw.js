215b3a4b9cd06885c25a1bfb02f2b237ff0d5948
FalcoN <48328425+IFalcoNI@users.noreply.github.com>
2021-11-26 02:31:21 +0200

    L6 HW ADDITIONAL DONE

--- JavaScript/L6_JS/JS/cw.js
+++ JavaScript/L6_JS/JS/cw.js
@@ -1,108 +1,101 @@
-// - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
-const findMin = (a, b, c) => {
-  return a <= b && a <= c
-    ? a + " a"
-    : b < a && b <= c
-    ? b + " b"
-    : c < a && c < b
-    ? c + " c"
-    : "";
-};
-// console.log(findMin(3, 2, 2));
+// - Дано список імен.
+let n1 = "Harry..Potter";
+let n2 = "Ron---Whisley";
+let n3 = "Hermione__Granger";
+// Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд
+// let n1 = 'Harry Potter'
+// let n2 = 'Ron Whisley'
+// let n3 = 'Hermione Granger'
 
-// - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
-const findMax = (a, b, c) => {
-  return a >= b && a >= c
-    ? a + " a"
-    : b > a && b >= c
-    ? b + " b"
-    : c > a && c > b
-    ? c + " c"
-    : "";
-};
-// console.log(findMax(3, 2, 7));
-
-// - створити функцію яка повертає найбільше число з масиву
-const maxFromArray = (array) => {
-  let max = array[0];
-  array.forEach((element) => {
-    if (element > max) {
-      max = element;
-    }
-  });
-  return max;
-};
-// console.log(maxFromArray([1, 6, 2, 9, 3]));
-
-// - створити функцію яка повертає найменьше число з масиву
-const minFromArray = (array) => {
-  let min = array[0];
-  array.forEach((element) => {
-    if (element < min) {
-      min = element;
-    }
-  });
-  return min;
-};
-// console.log(minFromArray([8, 2, 2, 9, 3]));
-
-// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
-const sumArray = (array) => {
-  let sum = 0;
-  array.forEach((element) => {
-    sum += element;
-  });
-  return sum;
-};
-// console.log(sumArray([8, 2, 2, 9, 3]));
-
-// - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
-const avgOfArray = (array) => {
-  let sum = 0;
-  array.forEach((element) => {
-    sum += element;
-  });
-  return sum / array.length;
-};
-// console.log(avgOfArray([8, 2, 2, 9, 3]));
-
-// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
-const minMaxArray = (array) => {
-  let min = array[0];
-  let max = array[0];
-  array.forEach((element) => {
-    if (element < min) {
-      min = element;
-    }
-    if (element > max) {
-      max = element;
-    }
-  });
-  console.log(max);
-  return min;
-};
-// console.log(minMaxArray([8, 2, 2, 9, 3]));
-
-// - створити функцію яка заповнює масив рандомними числами
-// (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
-// - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
-const randomArray = (len) => {
+// - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
+const randArray = (len) => {
   let array = [];
-  for (let i = 0; i < len; ++i) {
-    array[i] = Math.floor(Math.random() * 100);
-    // console.log(array[i]);
+  for (let index = 0; index < len; index++) {
+    array[index] = Math.floor(Math.random() * 100);
   }
   return array;
 };
-// console.log(randomArray(10));
-
-// - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
-const upsideDownArray = (array) => {
-  let revArray = [];
-  for (let i = array.length - 1, j = 0; i >= 0; --i, ++j) {
-    revArray[j] = array[i];
-  }
-  console.log(array);
-  return revArray;
+// console.log(randArray(10));
+// - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
+const ascendingSort = (a, b) => {
+  return a - b;
 };
-// console.log(upsideDownArray([1, 2, 3]));
+// console.log(randArray(10).sort(ascendingSort));
+
+// - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter, залишивши тільки парні числа
+const oddFilter = (x) => {
+  return x % 2 == 0;
+};
+// console.log(randArray(10).filter(oddFilter));
+
+// - створити масив рандомних цілих числових значень (або згенерувати, за допомоги попередньої функції) . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
+const arrayToString = (x) => {
+  return x.toString();
+};
+
+// console.log(randArray(10).map(arrayToString));
+// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+
+let nums = [11, 21, 3];
+
+const ascending = (a, b) => {
+  return a - b;
+};
+const descending = (a, b) => {
+  return b - a;
+};
+const sortNums = (direction) => {
+  let sorted;
+  direction === "ascending"
+    ? (sorted = nums.sort(ascending))
+    : direction === "descending"
+    ? (sorted = nums.sort(descending))
+    : (sorted = "Invalid input");
+
+  return sorted;
+};
+
+// console.log(sortNums("ascending")); // [3,11,21]
+// console.log(sortNums("descending")); // [21,11,3]
+// console.log(sortNums("abra cadabra"));
+
+// - є масив
+//  -- відсортувати його за спаданням за monthDuration
+//  -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+let coursesAndDurationArray = [
+  { title: "JavaScript Complex", monthDuration: 5 },
+  { title: "Java Complex", monthDuration: 6 },
+  { title: "Python Complex", monthDuration: 6 },
+  { title: "QA Complex", monthDuration: 4 },
+  { title: "FullStack", monthDuration: 7 },
+  { title: "Frontend", monthDuration: 4 },
+];
+
+const sortByMonthDuration = (a, b) => {
+  return b.monthDuration - a.monthDuration;
+};
+const monthFilter = (x) => {
+  return x.monthDuration > 5;
+};
+// console.log(
+//   coursesAndDurationArray.filter(monthFilter).sort(sortByMonthDuration)
+// );
+
+// - Напишите функцию cutString(str, n), которая делит строку на подстроки, состоящие из n символов.
+const cutString = (str, n) => {
+  let strArray = [];
+  let newStr = "";
+  let index = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (i % n == 0 && i != 0) {
+      console.log(newStr);
+      index++;
+    } else {
+      newStr += str[i];
+      strArray[index] = newStr;
+      newStr = "";
+    }
+  }
+  return strArray;
+};
+document.writeln(cutString("наслаждение", 3)); // [нас,лаж,ден,ие]
