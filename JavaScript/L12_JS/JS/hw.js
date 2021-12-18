fd360f1f7e8e6e3891324f081950c32b6a51c512
FalcoN <48328425+IFalcoNI@users.noreply.github.com>
2021-12-18 21:11:05 +0200

    L12 HW

--- JavaScript/L12_JS/JS/hw.js
+++ JavaScript/L12_JS/JS/hw.js
@@ -1,80 +1,52 @@
-// - Создать произвольный елемент с id = text.
-//  Используя JavaScript, сделайте так, чтобы при клике на кнопку исчезал элемент с id="text".
-let textDiv = document.getElementById("text");
-textDiv.innerText = "Hello";
-const deleteText = () => {
-  //   textDiv.innerText = "";
-  //   textDiv.hidden = true;
-  textDiv.style.display = "none";
-};
-// - Создайте кнопку, при клике на которую, она будет скрывать сама себя.
-const selfDestroy = () => {
-  document.getElementById("btn").hidden = true;
-};
-// - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
-let message = document.getElementById("message");
-const checkAge = () => {
-  let age = document.getElementById("inputAge").value;
-  if (+age < 18) {
-    message.innerText = "Ваш вік не відповідає вимогам сайту";
-  } else {
-    message.innerText = "Вітаємо";
-  }
-};
-// - Создайте меню, которое раскрывается/сворачивается при клике
-const toggleMenu = () => {
-  document.querySelector(".menuList").classList.toggle("hide");
-};
-// - Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
-// Вывести список комментариев в документ, каждый в своем блоке.
-//  Добавьте каждому комментарию по кнопке для сворачивания его body.
+// 1.
+// Отримати відповідь з цього ресурсу відповідь, та вивести в документ об'єкти. Застилізувати, за допомоги css, щоб отримати 5 елементів в рядку.
+// Для кожного елементу свій блок div.post
+// Всі характеристики повинні мати свої блоки всередені div.post
+// https://jsonplaceholder.typicode.com/posts
 
-let comments = [
-  {
-    username: "user1",
-    comment:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, earum!",
-  },
-  {
-    username: "user2",
-    comment:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, earum!",
-  },
-  {
-    username: "user3",
-    comment:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, earum!",
-  },
-  {
-    username: "user4",
-    comment:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, earum!",
-  },
-  {
-    username: "user5",
-    comment:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, earum!",
-  },
-];
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((posts) => {
+//     let postsContainer = document.createElement("div");
+//     postsContainer.setAttribute("class", "postsContainer");
+//     for (const itarator of posts) {
+//       let post = document.createElement("div");
+//       post.setAttribute("class", "post");
+//       post.innerHTML = `
+//         <div class="Id"> ${itarator.id}</div>
+//         <h2 class="Title"> ${itarator.title}</h2>
+//         <div class="Body"> ${itarator.body}</div>
+//       `;
+//       postsContainer.appendChild(post);
+//       document.body.appendChild(postsContainer);
+//     }
+//   });
 
-let commentsContainer = document.createElement("div");
-for (const item of comments) {
-  let commentBox = document.createElement("div");
-  //
-  let h2 = document.createElement("h2");
-  h2.innerText = item.username;
-  //
-  let p = document.createElement("p");
-  p.innerHTML = item.comment;
-  //
-  let btn = document.createElement("button");
-  btn.innerHTML = "Toggle comment";
-  //
-  btn.onclick = () => {
-    p.classList.toggle("hide");
-  };
-  commentBox.append(h2, p, btn);
-  //
-  commentsContainer.appendChild(commentBox);
-}
-document.getElementById("comments").appendChild(commentsContainer);
+// 2.
+// Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті.
+// Для кожного елементу свій блок div.comment
+// Всі характеристики повинні мати свої блоки всередені div.comment
+// https://jsonplaceholder.typicode.com/comments
+
+fetch("https://jsonplaceholder.typicode.com/comments")
+  .then((response) => {
+    return response.json();
+  })
+  .then((comments) => {
+    let commentsContainer = document.createElement("div");
+    commentsContainer.setAttribute("class", "commentsContainer");
+    for (const itarator of comments) {
+      let comment = document.createElement("div");
+      comment.setAttribute("class", "post");
+      comment.innerHTML = `
+        <div class="Id"> ${itarator.id}</div>
+        <h2 class="Name"> ${itarator.name}</h2>
+        <h3 class="Title"> ${itarator.email}</h3>
+        <div class="Body"> ${itarator.body}</div>
+      `;
+      commentsContainer.appendChild(comment);
+      document.body.appendChild(commentsContainer);
+    }
+  });
