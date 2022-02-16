95e77ea6887f7dbf04411d84bbbda0c56e09be82
FalcoN <48328425+IFalcoNI@users.noreply.github.com>
2022-02-16 21:16:35 +0200

    replace

--- NodeJS/L1_Node/main.js
+++ NodeJS/L1_Node/main.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const fs = require("fs");
+const Logger = require("nodemon/lib/utils/log");
 // Потім створити в вашому головному файлі (для прикладу app.js) два масиви з обєктами user ({. name: "Andrii", age: 22, city: "Lviv" }),  відповідно перший - onlineUsers, другий - inPersonUsers;
-let onlineUsers = [
+const onlineUsers = [
   { name: "Andrii", age: 22, city: "Lviv" },
   { name: "Elon", age: 99, city: "Zhmerynka" },
 ];
-let inPersonUsers = [
+const inPersonUsers = [
   { name: "Oleg", age: 22, city: "Lviv" },
   { name: "Anya", age: 20, city: "City" },
 ];
@@ -32,75 +33,65 @@ fs.mkdirSync(
 // і створити файли txt в папках (online, inPerson) в яких як дату покласти юзерів з ваших масивів, але щоб ваш файл виглядав як NAME: ім'я з обєкту і т.д і всі пункти з нового рядка.
 
 onlineUsers.forEach((user) => {
-  fs.appendFileSync(
-    path.join(__dirname, "main", "online", "onlineUsers.txt"),
+  fs.writeFile(
+    path.join(__dirname, "main", "online", user.name + ".txt"),
     `\nName: ${user.name} \nAge:${user.age} \nCity:${user.city}\n`,
-    { flag: "a" },
     (err) => {
       if (err) {
         console.log(err);
-        throw err;
       }
     }
   );
 });
 inPersonUsers.forEach((user) => {
-  fs.appendFileSync(
-    path.join(__dirname, "main", "inPerson", "inPersonUsers.txt"),
+  fs.writeFile(
+    path.join(__dirname, "main", "inPerson", user.name + ".txt"),
     `\nName: ${user.name} \nAge:${user.age} \nCity:${user.city}\n`,
-    { flag: "a" },
     (err) => {
       if (err) {
         console.log(err);
-        throw err;
       }
     }
   );
 });
 
-// Коли ви це виконаєте напишіть функцію яка буде міняти місцями юзерів з одного файлу і папки в іншу. (ті, що були в папці inPerson будуть в папці online)
-setTimeout(() => {
-  let swapData;
-  fs.readFile(
-    path.join(__dirname, "main", "inPerson", "inPersonUsers.txt"),
-    (err, data) => {
-      if (err) {
-        console.log(err);
-        throw err;
-      }
-      fs.readFile(
-        path.join(__dirname, "main", "online", "onlineUsers.txt"),
-        (err, data) => {
-          if (err) {
-            console.log(err);
-            throw err;
-          }
-          swapData = data;
-          console.log(swapData.toString());
-        }
-      );
-      fs.appendFileSync(
-        path.join(__dirname, "main", "online", "onlineUsers.txt"),
-        data.toString(),
-        { flag: "w" },
-        (err) => {
-          if (err) {
-            console.log(err);
-            throw err;
-          }
-        }
-      );
-      fs.appendFileSync(
-        path.join(__dirname, "main", "inPerson", "inPersonUsers.txt"),
-        "new",
-        { flag: "w" },
-        (err) => {
-          if (err) {
-            console.log(err);
-            throw err;
-          }
-        }
-      );
+// // Коли ви це виконаєте напишіть функцію яка буде міняти місцями юзерів з одного файлу і папки в іншу. (ті, що були в папці inPerson будуть в папці online)
+const swap = (firstFolder, secondFolder) => {
+  fs.readdir(path.join(__dirname, "main", firstFolder), (err, data) => {
+    if (err) {
+      console.log(err);
+      throw err;
     }
-  );
-}, 2000);
+    data.forEach((element) => {
+      fs.rename(
+        path.join(__dirname, "main", firstFolder, element),
+        path.join(__dirname, "main", secondFolder, element),
+        (err) => {
+          if (err) {
+            console.log(err);
+            throw err;
+          }
+        }
+      );
+    });
+  });
+  fs.readdir(path.join(__dirname, "main", secondFolder), (err, data) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    data.forEach((element) => {
+      fs.rename(
+        path.join(__dirname, "main", secondFolder, element),
+        path.join(__dirname, "main", firstFolder, element),
+        (err) => {
+          if (err) {
+            console.log(err);
+            throw err;
+          }
+        }
+      );
+    });
+  });
+};
+swap("inPerson", "online");
